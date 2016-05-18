77182c9dfa1895d99b9c3784c8d564c4d67c027a
Patlak <patlak@me.com>
2016-05-18 20:19:13 +0200

    Customers ArrayList

--- Protokoll/src/domain/Customer.java
+++ Protokoll/src/domain/Customer.java
@@ -1,5 +1,20 @@
 package domain;
 
-public class Customer {
+public class Customer 
+{
+	private int zahl;
+	
+	public Customer()
+	{
+		
+	}
 
+	public int getZahl() {
+		return zahl;
+	}
+
+	public void setZahl(int zahl) {
+		this.zahl = zahl;
+	}
+	
 }
